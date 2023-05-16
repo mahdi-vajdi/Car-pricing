64efcc2ad681640da4c02919516237b0008ae633
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-05-16 20:37:59 +0330

    Added controller, service and etc for user module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { SerializeInterceptor } from 'src/common/interceptors/serialize.interceptor';
+import { UserDto } from './dto/user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  createUser(@Body() body: CreateUserDto) {
+    this.usersService.create(body.email, body.password);
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.usersService.findOneByEmail(email);
+  }
+
+  @UseInterceptors(new SerializeInterceptor(UserDto))
+  @Get(':id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.usersService.findOneById(parseInt(id));
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: number, @Body() body: UpdateUserDto) {
+    return this.usersService.update(id, body);
+  }
+
+  @Delete(':id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id));
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User) private readonly repo: Repository<User>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+
+    return this.repo.save(user);
+  }
+
+  findOneById(id: number) {
+    return this.repo.findOneBy({ id });
+  }
+
+  findOneByEmail(email: string) {
+    return this.repo.findBy({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOneById(id);
+    if (!user)
+      throw new NotFoundException(`There is no user with the id: ${id}`);
+
+    Object.assign(user, attrs);
+
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOneById(id);
+    if (!user)
+      throw new NotFoundException(`There is no user with the id: ${id}`);
+
+    return this.repo.remove(user);
+  }
+}
