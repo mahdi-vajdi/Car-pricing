fd92360f9a588f109fbb2a22e40a07857322520e
Mahdi Vajdi <mahdivajdii@gmail.com>
2023-05-22 10:48:08 +0330

    Report Module -Added report controller, service and entity -Created relation between report and user entities

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { CreateReportDto } from './dto/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { Serialize } from '../common/interceptors/serialize.interceptor';
+import { reportDto } from './dto/report.dto';
+import { ApproveReportDto } from './dto/approve-report.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private readonly reportsService: ReportsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(reportDto)
+  create(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch(':id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportsService.changeApproval(+id, body.approved);
+  }
+}
