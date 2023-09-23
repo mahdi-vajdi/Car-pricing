d2a59e8104a60e27a03b06aff8305683ff8559bc
mahdi-vajdi <mahdivajdii@gmail.com>
2023-09-23 11:04:33 +0330

    Added Joi validation for env fields

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,23 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        JWT_SECRET: Joi.string().required(),
+        MYSQL_ROOT_PASSWORD: Joi.string().required(),
+        MYSQL_HOST: Joi.string().required(),
+        MYSQL_PORT: Joi.string().required(),
+        MYSQL_DATABASE: Joi.string().required(),
+        MYSQL_USERNAME: Joi.string().required(),
+        MYSQL_PASSWORD: Joi.string().required(),
+        MYSQL_SYNCHRONIZE: Joi.boolean().required(),
+      }),
     }),
     DatabaseModule,
     UsersModule,
